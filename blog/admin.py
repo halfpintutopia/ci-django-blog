981bdd967a4a77ff65e9cf494b612d19d23aef04
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-29 18:36:59 +0200

    fix: change search field into list

--- blog/admin.py
+++ blog/admin.py
@@ -8,5 +8,5 @@ class PostAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     list_display = ('title', 'slug', 'status', 'created_on')
-    search_fields = ('title', 'content')
+    search_fields = ['title', 'content']
     summernote_fields = 'content'
