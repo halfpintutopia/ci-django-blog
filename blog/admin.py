dde178237c315f8f7e82f2220346e093eeda046f
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-29 18:40:52 +0200

    feat: add list, filter and search to comment admin page

--- blog/admin.py
+++ blog/admin.py
@@ -10,3 +10,10 @@ class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     summernote_fields = 'content'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
